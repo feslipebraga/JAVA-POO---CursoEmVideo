3c956c23bbc19b4d6fc1bc792eddfb360d0113c9
feslipebraga <feslipebraga@gmail.com>
2023-10-28 23:28:18 -0300

    att

---
+++ UltraEmojiCombat/src/combat/Combat.java
@@ -0,0 +1,40 @@
+package combat;
+
+public class Combat {
+    public static void main(String[] args) {
+    	// LUTADOR
+        Lutador l[] = new Lutador[6];
+        
+        l[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.6f, 12, 2, 1);
+        l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
+        l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+        l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
+        
+        // LUTA
+        Luta UEC[] = new Luta[3];
+        
+        for (int i = 0; i < UEC.length; i++) {
+            UEC[i] = new Luta();
+        }
+      
+        UEC[0].marcarLuta(l[0], l[1]);
+        UEC[0].lutar();
+        
+        l[0].status();
+        l[1].status();
+        
+        UEC[1].marcarLuta(l[2], l[3]);
+        UEC[1].lutar();
+        
+        l[2].status();
+        l[3].status();
+        
+        UEC[2].marcarLuta(l[4], l[5]);
+        UEC[2].lutar();
+        
+        l[4].status();
+        l[5].status();
+    }
+}
